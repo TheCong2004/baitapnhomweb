9394748a46111f0bd3b3b94e8a8c94aeab3de7fa
nguyentran344 <nguyentran12022@gmail.com>
2025-09-12 21:53:14 +0700

    Thêm tính năng đăng ký, đăng nhập người dùng với xác thực cơ bản

--- src/main/java/com/example/webnhom/repository/UserDemoRepository.java
+++ src/main/java/com/example/webnhom/repository/UserDemoRepository.java
@@ -1,9 +1,12 @@
 package com.example.webnhom.repository;
+
 import com.example.webnhom.model.UserDemo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.Optional;
 
 @Repository
 
 public interface UserDemoRepository extends JpaRepository<UserDemo, Long> {
+    Optional<UserDemo> findByEmail(String email);
 }
